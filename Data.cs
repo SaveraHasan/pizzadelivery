8385cf172ed2dc1ac166b5c287bd88d607a5c313
Savera hasan <saverahasan5@gmail.com>
2022-07-20 16:43:56 +0500

    Add project files.

---
+++ Data.cs
@@ -0,0 +1,16 @@
+﻿namespace WindowsFormsApplication3
+{
+    internal class Data
+    {
+       
+
+        public string Name { get; set; }
+        public string Instruction { get; internal set; }
+        public string Contact { get; internal set; }
+        public string Email { get; internal set; }
+        public string Amount { get; internal set; }
+        public string PaymentMethod { get; internal set; }
+        public string Address { get; internal set; }
+        public string Province { get; internal set; }
+    }
+}
